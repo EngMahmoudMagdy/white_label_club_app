df2e2a77343b09ed4e4464cabe5f29d60507c28e
m3aarfRepo <133111792+m3aarfRepo@users.noreply.github.com>
2024-02-29 21:16:29 +0200

    small fix

--- lib/features/Intro/presentation/widgets/onboarding_page/body.dart
+++ lib/features/Intro/presentation/widgets/onboarding_page/body.dart
@@ -1,8 +1,6 @@
-import 'package:clean_architecture/core/util/color_resources.dart';
-import 'package:clean_architecture/core/util/size_config.dart';
+
 import 'package:clean_architecture/features/Intro/presentation/widgets/onboarding_page/bottom_widget.dart';
 import 'package:clean_architecture/features/Intro/presentation/widgets/onboarding_page/top_widget.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
